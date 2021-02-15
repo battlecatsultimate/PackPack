c09370eb648385ee5786326a2cf14d3622a94083
MandarinSmell <mandarinsmellbcu@gmail.com>
2021-02-15 16:42:46 +0900

    fix lower case functions

--- src/main/java/mandarin/packpack/supporter/bc/EntityFilter.java
+++ src/main/java/mandarin/packpack/supporter/bc/EntityFilter.java
@@ -4,6 +4,9 @@ import common.CommonStatic;
 import common.pack.UserProfile;
 import common.util.Data;
 import common.util.lang.MultiLangCont;
+import common.util.stage.MapColc;
+import common.util.stage.Stage;
+import common.util.stage.StageMap;
 import common.util.unit.Enemy;
 import common.util.unit.Form;
 import common.util.unit.Unit;
@@ -28,7 +31,7 @@ public class EntityFilter {
                         fname.append(MultiLangCont.get(f));
                     }
 
-                    if(fname.toString().toLowerCase(Locale.ROOT).contains(name.toLowerCase(Locale.ROOT))) {
+                    if(fname.toString().toLowerCase(Locale.ENGLISH).contains(name.toLowerCase(Locale.ENGLISH))) {
                         res.add(f);
                         break;
                     }
@@ -55,9 +58,146 @@ public class EntityFilter {
                     ename.append(MultiLangCont.get(e));
                 }
 
-                if(ename.toString().toLowerCase(Locale.ROOT).contains(name.toLowerCase(Locale.ROOT))) {
+                if(ename.toString().toLowerCase(Locale.ENGLISH).contains(name.toLowerCase(Locale.ENGLISH))) {
                     res.add(e);
                     break;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static ArrayList<Stage> findStageWithName(String[] names) {
+        ArrayList<Stage> res = new ArrayList<>();
+
+        for(MapColc mc : MapColc.values()) {
+            if(mc == null)
+                continue;
+
+            if(searchMapColc(names) && names[0] != null && !names[0].isBlank()) {
+                for(int i = 0; i < 4; i++) {
+                    CommonStatic.getConfig().lang = i;
+
+                    String mcName = MultiLangCont.get(mc);
+
+                    if(mcName == null || mcName.isBlank())
+                        continue;
+
+                    if(!mcName.isBlank()) {
+                        if(mcName.toLowerCase(Locale.ENGLISH).contains(names[0].toLowerCase(Locale.ENGLISH))) {
+                            for(StageMap stm : mc.maps.getList()) {
+                                if(stm == null)
+                                    continue;
+
+                                for(Stage st : stm.list.getList()) {
+                                    if(st == null)
+                                        continue;
+
+                                    res.add(st);
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                continue;
+            }
+
+            for(StageMap stm : mc.maps.getList()) {
+                if(stm == null)
+                    continue;
+
+                if(searchStageMap(names) && names[1] != null && !names[1].isBlank()) {
+                    for(int i = 0; i < 4; i++) {
+                        CommonStatic.getConfig().lang = i;
+
+                        boolean s0 = true;
+
+                        if(names[0] != null && !names[0].isBlank()) {
+                            String mcName = MultiLangCont.get(mc);
+
+                            if(mcName != null && !mcName.isBlank()) {
+                                s0 = mcName.toLowerCase(Locale.ENGLISH).contains(names[0].toLowerCase(Locale.ENGLISH));
+                            }
+                        }
+
+                        boolean s1 = true;
+
+                        if(names[1] != null && !names[1].isBlank()) {
+                            String stmName = MultiLangCont.get(stm);
+
+                            if(stmName == null || stmName.isBlank())
+                                continue;
+
+                            if(!stmName.isBlank()) {
+                                s1 = stmName.toLowerCase(Locale.ENGLISH).contains(names[1].toLowerCase(Locale.ENGLISH));
+                            }
+                        }
+
+                        if(s0 && s1) {
+                            for(Stage st : stm.list.getList()) {
+                                if(st == null)
+                                    continue;
+
+                                res.add(st);
+                            }
+
+                            break;
+                        }
+                    }
+
+                    continue;
+                }
+
+                for(Stage st : stm.list.getList()) {
+                    for(int i = 0; i < 4; i++) {
+                        if(names[2] == null)
+                            continue;
+
+                        String stName = MultiLangCont.get(st);
+
+                        if(stName == null || stName.isBlank())
+                            continue;
+
+                        boolean s0 = true;
+
+                        if(names[0] != null && !names[0].isBlank()) {
+                            String mcName = MultiLangCont.get(mc);
+
+                            if(mcName != null && !mcName.isBlank()) {
+                                s0 = mcName.toLowerCase(Locale.ENGLISH).contains(names[0].toLowerCase(Locale.ENGLISH));
+                            }
+                        }
+
+                        boolean s1 = true;
+
+                        if(names[1] != null && !names[1].isBlank()) {
+                            String stmName = MultiLangCont.get(stm);
+
+                            if(stmName != null && !stmName.isBlank()) {
+                                s1 = stmName.toLowerCase(Locale.ENGLISH).contains(names[1].toLowerCase(Locale.ENGLISH));
+                            }
+                        }
+
+                        boolean s2 = false;
+
+                        if(!stName.isBlank()) {
+                            s2 = stName.toLowerCase(Locale.ENGLISH).contains(names[2].toLowerCase(Locale.ENGLISH));
+                        }
+
+                        String id = mc.getSID()+"-"+Data.trio(stm.id.id)+"-"+Data.trio(st.id.id)+" "+mc.getSID()+"-"+stm.id.id+"-"+st.id.id;
+
+                        boolean s3 = id.toLowerCase(Locale.ENGLISH).contains(names[2].toLowerCase(Locale.ENGLISH));
+
+                        if(s0 && s1 && (s2 || s3)) {
+                            res.add(st);
+
+                            break;
+                        }
+                    }
                 }
             }
         }
@@ -70,5 +210,13 @@ public class EntityFilter {
             return "0"+i;
         else
             return String.valueOf(i);
+    }
+
+    private static boolean searchMapColc(String[] names) {
+        return names[1] == null && names[2] == null;
+    }
+
+    private static boolean searchStageMap(String[] names) {
+        return names[2] == null;
     }
 }
