9362772f1004c5fac857f0d7c93f4cae9737c9af
MandarinSmell <mandarinsmellbcu@gmail.com>
2021-03-05 03:25:32 +0900

    fix parameter order when drawing gif

--- src/main/java/mandarin/packpack/supporter/bc/ImageDrawing.java
+++ src/main/java/mandarin/packpack/supporter/bc/ImageDrawing.java
@@ -455,7 +455,7 @@ public class ImageDrawing {
         return gif;
     }
 
-    public static File drawAnimGif(EAnimD<?> anim, Message msg, double siz, boolean debug, int lang, int limit) throws Exception {
+    public static File drawAnimGif(EAnimD<?> anim, Message msg, double siz, boolean debug, int limit, int lang) throws Exception {
         File temp = new File("./temp");
 
         if(!temp.exists()) {
