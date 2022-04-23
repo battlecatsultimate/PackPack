ee9c3a44d75c62c399d655b58e61c52978dc77eb
MandarinSmell <mandarinsmellbcu@gmail.com>
2022-04-23 18:54:35 +0900

    fix ability displaying

--- src/main/java/mandarin/packpack/supporter/bc/Interpret.java
+++ src/main/java/mandarin/packpack/supporter/bc/Interpret.java
@@ -16,22 +16,28 @@ import java.util.List;
 
 @SuppressWarnings("ForLoopReplaceableByForEach")
 public class Interpret extends Data {
-    public static final String[] TRAIT = {"data_red", "data_float", "data_black", "data_metal", "data_angel", "data_alien", "data_zombie", "data_demon", "data_relic", "data_white", "data_eva", "data_witch", "data_baron", "data_beast", "data_baset"};
+    public static final String[] TRAIT = {
+            "data_red", "data_float", "data_black", "data_metal", "data_angel", "data_alien", "data_zombie",
+            "data_demon", "data_relic", "data_white", "data_eva", "data_witch", "data_baron", "data_beast", "data_baset"
+    };
 
-    public static final String[] ABIS = {"data_strong", "data_resistant", "data_massive", "data_attackon", "data_extramon",
-            "data_basedest", "data_abimetal", "data_waveshie", "data_imusnipe", "data_imustoptt",
-            "data_ghost", "data_zombiekill", "data_witchkill", "data_suicide", "data_imutheme", "data_evakill",
-            "data_imuboss", "data_insanetou", "data_insanedmg", "data_baronkiller", "data_corpsekiller"};
+    public static final String[] ABIS = {
+            "data_strong", "data_resistant", "data_massive", "data_attackon", "data_abimetal", "data_waveshie",
+            "data_imusnipe", "data_imustoptt", "data_ghost", "data_zombiekill", "data_witchkill", "data_suicide",
+            "data_imutheme", "data_evakill", "data_imuboss", "data_insanetou", "data_insanedmg", "data_baronkiller",
+            "data_corpsekiller"
+    };
 
-    public static final String[] PROCIND = {"WEAK", "STOP", "SLOW", "KB", "WARP", "CURSE", "IMUATK",
-            "STRONG", "LETHAL", "CRIT", "BREAK", "SHIELDBREAK", "SATK", "MINIWAVE", "WAVE", "VOLC", "BSTHUNT", "IMUWEAK",
-            "IMUSTOP", "IMUSLOW", "IMUKB", "IMUWAVE", "IMUVOLC", "IMUWARP", "IMUCURSE", "IMUPOIATK",
-            "POIATK", "DEMONSHIELD", "DEATHSURGE", "BURROW", "REVIVE", "SNIPER", "SEAL", "TIME", "SUMMON", "MOVEWAVE", "THEME",
-            "POISON", "BOSS", "ARMOR", "SPEED", "COUNTER", "DMGCUT", "DMGCAP", "CRITI", "IMUSEAL", "IMUPOI", "IMUSUMMON",
-            "IMUMOVING", "IMUARMOR", "IMUSPEED"};
+    public static final String[] PROCIND = {
+            "WEAK", "STOP", "SLOW", "KB", "WARP", "CURSE", "IMUATK", "STRONG", "LETHAL", "CRIT", "BREAK", "SHIELDBREAK",
+            "SATK", "MINIWAVE", "WAVE", "VOLC", "BSTHUNT", "IMUWEAK", "IMUSTOP", "IMUSLOW", "IMUKB", "IMUWAVE", "IMUVOLC",
+            "IMUWARP", "IMUCURSE", "IMUPOIATK", "POIATK", "DEMONSHIELD", "DEATHSURGE", "BURROW", "REVIVE", "SNIPER", "SEAL",
+            "TIME", "SUMMON", "MOVEWAVE", "THEME", "POISON", "BOSS", "ARMOR", "SPEED", "COUNTER", "DMGCUT", "DMGCAP",
+            "CRITI", "IMUSEAL", "IMUPOI", "IMUSUMMON", "IMUMOVING", "IMUARMOR", "IMUSPEED"
+    };
 
     public static final int[] P_INDEX = {P_WEAK, P_STOP, P_SLOW, P_KB, P_WARP, P_CURSE, P_IMUATK,
-            P_STRONG, P_LETHAL, P_CRIT, P_BREAK, P_SHIELDBREAK, P_SATK, P_MINIWAVE, P_WAVE, P_VOLC, P_BSTHUNT, P_IMUWEAK,
+            P_STRONG, P_LETHAL, P_ATKBASE, P_CRIT, P_BREAK, P_SHIELDBREAK, P_SATK, P_BOUNTY, P_MINIWAVE, P_WAVE, P_VOLC, P_BSTHUNT, P_IMUWEAK,
             P_IMUSTOP, P_IMUSLOW, P_IMUKB, P_IMUWAVE, P_IMUVOLC, P_IMUWARP, P_IMUCURSE, P_IMUPOIATK,
             P_POIATK, P_DEMONSHIELD, P_DEATHSURGE, P_BURROW, P_REVIVE, P_SNIPER, P_SEAL, P_TIME, P_SUMMON, P_MOVEWAVE, P_THEME,
             P_POISON, P_BOSS, P_ARMOR, P_COUNTER, P_DMGCUT, P_DMGCUT, P_SPEED, P_CRITI, P_IMUSEAL, P_IMUPOI, P_IMUSUMMON, P_IMUMOVING,
@@ -101,23 +107,17 @@ public class Interpret extends Data {
                         case 2:
                             ab += LangID.getStringByID("data_add2", lang);
                             break;
-                        case 4:
+                        case 10:
                             ab += LangID.getStringByID("data_add3", lang);
                             break;
-                        case 5:
+                        case 13:
                             ab += LangID.getStringByID("data_add4", lang);
                             break;
-                        case 12:
+                        case 15:
                             ab += LangID.getStringByID("data_add5", lang);
                             break;
-                        case 15:
+                        case 16:
                             ab += LangID.getStringByID("data_add6", lang);
-                            break;
-                        case 17:
-                            ab += LangID.getStringByID("data_add7", lang);
-                            break;
-                        case 18:
-                            ab += LangID.getStringByID("data_add8", lang);
                             break;
                     }
 
