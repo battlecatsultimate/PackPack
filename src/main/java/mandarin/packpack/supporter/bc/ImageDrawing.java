8da5b685824d341472e2becac800c781c92ce02c
MandarinSmell <mandarinsmellbcu@gmail.com>
2022-05-28 23:35:29 +0900

    fix bg effect drawing code, update bg data

--- src/main/java/mandarin/packpack/supporter/bc/ImageDrawing.java
+++ src/main/java/mandarin/packpack/supporter/bc/ImageDrawing.java
@@ -83,7 +83,9 @@ public class ImageDrawing {
             4400,
             5400,
             4900,
-            4400
+            4400,
+            5400,
+            5000
     };
 
     private static Font titleFont;
@@ -192,8 +194,8 @@ public class ImageDrawing {
             BackgroundEffect effect = CommonStatic.getBCAssets().bgEffects.get(bg.effect);
 
             int len = (int) ((w / ratio - 400) / CommonStatic.BattleConst.ratio);
-            double bgHeight = h / ratio;
-            double midH = h * groundRatio / ratio;
+            int bgHeight = (int) (h / bgAnimRatio);
+            int midH = (int) (h * groundRatio / bgAnimRatio);
 
             effect.initialize(len, bgHeight, midH, bg);
 
@@ -201,7 +203,7 @@ public class ImageDrawing {
                 effect.update(len, bgHeight, midH);
             }
 
-            P base = P.newP(0, BackgroundEffect.BGHeight * 3 * ratio - h * 0.9);
+            P base = P.newP((int) (h * 0.0025), (int) (BackgroundEffect.BGHeight * 3 * bgAnimRatio - h * 0.905));
 
             effect.preDraw(g, base, ratio, midH);
             effect.postDraw(g, base, ratio, midH);
@@ -255,8 +257,8 @@ public class ImageDrawing {
         BackgroundEffect eff = CommonStatic.getBCAssets().bgEffects.get(bg.effect);
 
         int len = (int) ((w / bgAnimRatio - 400) / CommonStatic.BattleConst.ratio);
-        double bgHeight = h / bgAnimRatio;
-        double midH = h * groundRatio / bgAnimRatio;
+        int bgHeight = (int) (h / bgAnimRatio);
+        int midH = (int) (h * groundRatio / bgAnimRatio);
 
         eff.initialize(len, bgHeight, midH, bg);
 
@@ -337,7 +339,7 @@ public class ImageDrawing {
                 g.gradRect(0, 0, w, h - groundHeight - lowHeight, 0, 0, bg.cs[0], 0, h - groundHeight - lowHeight, bg.cs[1]);
             }
 
-            P base = P.newP(0, BackgroundEffect.BGHeight * 3 * bgAnimRatio - h * 0.9);
+            P base = P.newP((int) (h * 0.0025), (int) (BackgroundEffect.BGHeight * 3 * bgAnimRatio - h * 0.905));
 
             eff.preDraw(g, base, bgAnimRatio, midH);
             eff.postDraw(g, base, bgAnimRatio, midH);
