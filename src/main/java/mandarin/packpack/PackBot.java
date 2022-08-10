714461129d83c3a4c3b5ac8bf1ac11c523684686
MandarinSmell <mandarinsmellbcu@gmail.com>
2022-08-10 19:10:24 +0900

    remove unncessary import

--- src/main/java/mandarin/packpack/PackBot.java
+++ src/main/java/mandarin/packpack/PackBot.java
@@ -16,7 +16,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
