2e75a14c2e8d11444dd18fa5b98d24598e68695c
MandarinSmell <mandarinsmellbcu@gmail.com>
2022-09-21 01:10:35 +0900

    add confirmation before clearing cache

--- src/main/java/mandarin/packpack/commands/server/ClearCache.java
+++ src/main/java/mandarin/packpack/commands/server/ClearCache.java
@@ -4,6 +4,9 @@ import mandarin.packpack.commands.ConstraintCommand;
 import mandarin.packpack.supporter.StaticStore;
 import mandarin.packpack.supporter.lang.LangID;
 import mandarin.packpack.supporter.server.data.IDHolder;
+import mandarin.packpack.supporter.server.holder.ConfirmButtonHolder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 
@@ -19,8 +22,16 @@ public class ClearCache extends ConstraintCommand {
         if(ch == null)
             return;
 
-        StaticStore.imgur.clear();
+        Message res = registerConfirmButtons(ch.sendMessage("Are you sure you want to clear cache? This cannot be undone"), lang).complete();
 
-        ch.sendMessage(LangID.getStringByID("clearcache_cleared", lang)).queue();
+        Member m = getMember(event);
+
+        if(m != null) {
+            StaticStore.putHolder(m.getId(), new ConfirmButtonHolder(res, getMessage(event), ch.getId(), () -> {
+                StaticStore.imgur.clear();
+
+                ch.sendMessage(LangID.getStringByID("clearcache_cleared", lang)).queue();
+            }, lang));
+        }
     }
 }
