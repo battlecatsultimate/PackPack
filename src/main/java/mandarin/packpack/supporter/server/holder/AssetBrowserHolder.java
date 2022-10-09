a44786214c72cc2868d64ec8c669ade09005342d
MandarinSmell <mandarinsmellbcu@gmail.com>
2022-10-09 19:48:17 +0900

    add finisher to AssetBrowserHolder

--- src/main/java/mandarin/packpack/supporter/server/holder/AssetBrowserHolder.java
+++ src/main/java/mandarin/packpack/supporter/server/holder/AssetBrowserHolder.java
@@ -38,6 +38,8 @@ public class AssetBrowserHolder extends SearchHolder implements Comparator<VFile
         }
 
         files.addAll(fileList);
+
+        registerAutoFinish(this, msg, author, lang, FIVE_MIN);
     }
 
     @Override
