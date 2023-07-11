d7ca98088471fd57a00251b4461b56239f927ec7
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2023-07-11 12:42:44 +0900

    consider trait talent too

--- src/main/java/mandarin/card/supporter/filter/TraitFilter.kt
+++ src/main/java/mandarin/card/supporter/filter/TraitFilter.kt
@@ -8,6 +8,12 @@ class TraitFilter(private val trait: Trait, amount: Int, name: String) : Filter(
     override fun filter(card: Card): Boolean {
         val uber = UserProfile.getBCData().units[card.unitID]
 
-        return uber.forms.any { f -> f.du.traits.contains(trait) }
+        return uber.forms.any { f ->
+            if (f.fid == 2 && f.du.pCoin != null) {
+                f.du.pCoin.full.traits.contains(trait)
+            } else {
+                f.du.traits.contains(trait)
+            }
+        }
     }
 }
