013fd624d5edfd1ea87fcf0548d7117306fe4d85
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2023-08-09 17:48:20 +0900

    support 12.5 BG effect

--- src/test/java/TestUnit.kt
+++ src/test/java/TestUnit.kt
@@ -1,3 +1,4 @@
+import common.util.Data
 import java.io.File
 
 class TestUnit {
@@ -16,18 +17,10 @@ class TestUnit {
 
                 cards.sortBy { it.name.split("-")[0].toInt() }
 
-                when(t.name) {
-                    "Tier 1" -> println("---------- Tier 1 ----------")
-                    "Tier 2" -> println("---------- Tier 2 ----------")
-                    "Tier 3" -> println("---------- Tier 3 ----------")
-                    "Tier 4" -> println("---------- Tier 4 ----------")
-                    else -> throw IllegalStateException("Invalid tier type ${t.name} found")
-                }
-
                 for(card in cards) {
                     val nameData = card.name.replace(".png", "").split(Regex("-"), 2)
 
-                    println(nameData[0] + " - " + nameData[1])
+                    println("210660${nameData[0]} 3 --${nameData[1]}")
                 }
             }
         }
