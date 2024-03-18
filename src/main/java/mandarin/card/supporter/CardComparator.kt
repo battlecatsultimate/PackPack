853e9590a6c58e09b1db6dfe877f9a6e01d19d12
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2024-03-18 16:17:33 +0900

    put t0 back in the list

--- src/main/java/mandarin/card/supporter/CardComparator.kt
+++ src/main/java/mandarin/card/supporter/CardComparator.kt
@@ -8,8 +8,17 @@ class CardComparator : Comparator<Card> {
         o1 ?: return -1
         o2 ?: return 1
 
+        if (o1.unitID < 0 && o2.unitID >= 0)
+            return 1
+
+        if (o1.unitID >= 0 && o2.unitID < 0)
+            return -1
+
         return if (o1.tier == o2.tier) {
-            o1.unitID.compareTo(o2.unitID)
+            if (o1.unitID < 0 && o2.unitID < 0)
+                -o1.unitID.compareTo(o2.unitID)
+            else
+                o1.unitID.compareTo(o2.unitID)
         } else {
             o1.tier.ordinal.compareTo(o2.tier.ordinal)
         }
