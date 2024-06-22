a10df5e97d4b01a8d3df4ca21f3882a93f132cb1
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2024-06-22 17:16:13 +0900

    remove warning

--- src/main/java/mandarin/card/supporter/holder/moderation/GuildListHolder.kt
+++ src/main/java/mandarin/card/supporter/holder/moderation/GuildListHolder.kt
@@ -3,7 +3,6 @@ package mandarin.card.supporter.holder.moderation
 import mandarin.packpack.supporter.EmojiStore
 import mandarin.packpack.supporter.server.holder.component.ComponentHolder
 import mandarin.packpack.supporter.server.holder.component.search.SearchHolder
-import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.interaction.component.GenericComponentInteractionCreateEvent
 import net.dv8tion.jda.api.interactions.components.ActionRow
