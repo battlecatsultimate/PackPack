1af90cee4c2194945a39844d3bc5ea092638cf07
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2024-06-26 09:02:46 +0900

    add t0/1/2 commands

--- src/main/java/mandarin/card/CardBot.kt
+++ src/main/java/mandarin/card/CardBot.kt
@@ -425,6 +425,12 @@ object CardBot : ListenerAdapter() {
             "${globalPrefix}mi"-> ModifyInventory().execute(event)
             "${globalPrefix}activate" -> Activate().execute(event)
             "${globalPrefix}equip" -> Equip().execute(event)
+            "${globalPrefix}checkt0",
+            "${globalPrefix}t0" -> Check(CardData.Tier.SPECIAL).execute(event)
+            "${globalPrefix}checkt1",
+            "${globalPrefix}t1" -> Check(CardData.Tier.COMMON).execute(event)
+            "${globalPrefix}checkt2",
+            "${globalPrefix}t2" -> Check(CardData.Tier.UNCOMMON).execute(event)
             "${globalPrefix}checkt3",
             "${globalPrefix}t3" -> Check(CardData.Tier.ULTRA).execute(event)
             "${globalPrefix}checkt4",
