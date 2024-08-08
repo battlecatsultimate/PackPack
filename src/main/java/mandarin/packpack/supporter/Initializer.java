d2f5207dd7e37c81dfedbc6c221042ecfcf223e4
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2024-08-08 11:15:01 +0900

    prevent putting empty texts

--- src/main/java/mandarin/packpack/supporter/Initializer.java
+++ src/main/java/mandarin/packpack/supporter/Initializer.java
@@ -28,6 +28,7 @@ import mandarin.packpack.supporter.server.data.TreasureHolder;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 
@@ -219,7 +220,12 @@ public class Initializer {
                                 int j = 0;
 
                                 while (j < Math.min(str.length - 1, u.forms.length)) {
-                                    MultiLangCont.getStatic().FNAME.put(f, u.forms[j], str[j + 1].trim());
+                                    String name = str[j + 1].trim();
+
+                                    if (!name.isBlank()) {
+                                        MultiLangCont.getStatic().FNAME.put(f, u.forms[j], name);
+                                    }
+
                                     j++;
                                 }
 
@@ -246,7 +252,11 @@ public class Initializer {
 
                                 while (j < Math.min(u.forms.length, str.length - 1)) {
                                     String[] lines = str[j + 1].trim().split("<br>");
-                                    MultiLangCont.getStatic().FEXP.put(f, u.forms[j], lines);
+
+                                    if (Arrays.stream(lines).anyMatch(l -> !l.isBlank())) {
+                                        MultiLangCont.getStatic().FEXP.put(f, u.forms[j], lines);
+                                    }
+
                                     j++;
                                 }
 
@@ -299,7 +309,9 @@ public class Initializer {
 
                                 String name = str[1].trim();
 
-                                MultiLangCont.getStatic().COMNAME.put(f, c, name);
+                                if (!name.isBlank()) {
+                                    MultiLangCont.getStatic().COMNAME.put(f, c, name);
+                                }
                             }
                         }
                         case "EnemyName.txt" -> {
@@ -330,7 +342,9 @@ public class Initializer {
                                     name = str[1].trim();
                                 }
 
-                                MultiLangCont.getStatic().ENAME.put(f, e, name);
+                                if (!name.isBlank()) {
+                                    MultiLangCont.getStatic().ENAME.put(f, e, name);
+                                }
                             }
                         }
                         case "EnemyExplanation.txt" -> {
@@ -347,7 +361,9 @@ public class Initializer {
 
                                 String[] lines = str[1].trim().split("<br>");
 
-                                MultiLangCont.getStatic().EEXP.put(f, e, lines);
+                                if (Arrays.stream(lines).anyMatch(l -> !l.isBlank())) {
+                                    MultiLangCont.getStatic().EEXP.put(f, e, lines);
+                                }
                             }
                         }
                         case "StageName.txt" -> {
@@ -377,6 +393,7 @@ public class Initializer {
 
                                 if (ids.length == 1) {
                                     MultiLangCont.getStatic().MCNAME.put(f, mc, name);
+
                                     continue;
                                 }
 
@@ -389,6 +406,7 @@ public class Initializer {
 
                                 if (ids.length == 2) {
                                     MultiLangCont.getStatic().SMNAME.put(f, stm, name);
+
                                     continue;
                                 }
 
@@ -413,6 +431,9 @@ public class Initializer {
                                     continue;
 
                                 String name = str[1].trim();
+
+                                if (name.isBlank())
+                                    continue;
 
                                 String[] ids = str[0].trim().split("\\|");
 
