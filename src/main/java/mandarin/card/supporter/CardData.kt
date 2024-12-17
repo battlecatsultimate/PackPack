de7b7b5ea07ebddef4a4dbb834212cf379055003
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2024-12-18 00:21:10 +0900

    add 777

--- src/main/java/mandarin/card/supporter/CardData.kt
+++ src/main/java/mandarin/card/supporter/CardData.kt
@@ -147,7 +147,7 @@ object CardData {
             //Halloweens
             arrayOf(229, 230, 302, 570, 683, 772),
             //X-Mas
-            arrayOf(241, 242, 243, 310, 526, 584, 687),
+            arrayOf(241, 242, 243, 310, 526, 584, 687, 777),
 
             //Collaboration
 
