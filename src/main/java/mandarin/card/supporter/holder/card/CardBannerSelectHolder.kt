0d764dd2438ff1a9a3981a927623b16c3c8df38e
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2025-03-04 23:59:52 +0900

    add page system to CardBannerSelectHolder

--- src/main/java/mandarin/card/supporter/holder/card/CardBannerSelectHolder.kt
+++ src/main/java/mandarin/card/supporter/holder/card/CardBannerSelectHolder.kt
@@ -15,15 +15,44 @@ import net.dv8tion.jda.api.interactions.callbacks.IMessageEditCallback
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import net.dv8tion.jda.api.interactions.components.LayoutComponent
 import net.dv8tion.jda.api.interactions.components.buttons.Button
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
 import net.dv8tion.jda.api.interactions.components.selections.SelectOption
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu
+import kotlin.math.max
 import kotlin.math.min
 
 class CardBannerSelectHolder(author: Message, userID: String, channelID: String, message: Message, private val card: Card) : ComponentHolder(author, userID, channelID, message, CommonStatic.Lang.Locale.EN) {
     private var page = 0
+        set(value) {
+            field = value
+
+            val totalPage = getTotalPage(CardData.banners.size)
+
+            field = max(0, min(field, totalPage - 1))
+        }
 
     override fun onEvent(event: GenericComponentInteractionCreateEvent) {
         when(event.componentId) {
+            "prev" -> {
+                page--
+
+                applyResult(event)
+            }
+            "prev10" -> {
+                page -= 10
+
+                applyResult(event)
+            }
+            "next" -> {
+                page++
+
+                applyResult(event)
+            }
+            "next10" -> {
+                page += 10
+
+                applyResult(event)
+            }
             "none" -> {
                 card.banner = Banner.NONE
 
@@ -111,6 +140,42 @@ class CardBannerSelectHolder(author: Message, userID: String, channelID: String,
             options.add(SelectOption.of("a", "a"))
 
             result.add(ActionRow.of(StringSelectMenu.create("banner").addOptions(options).setPlaceholder("No banner").setDisabled(true).build()))
+        }
+
+        val totalPage = getTotalPage(CardData.banners.size)
+
+        if (CardData.banners.size > SearchHolder.PAGE_CHUNK) {
+            val buttons = ArrayList<Button>()
+
+            if(totalPage > 10) {
+                if(page - 10 < 0) {
+                    buttons.add(Button.of(ButtonStyle.SECONDARY, "prev10", "Previous 10 Pages", EmojiStore.TWO_PREVIOUS).asDisabled())
+                } else {
+                    buttons.add(Button.of(ButtonStyle.SECONDARY, "prev10", "Previous 10 Pages", EmojiStore.TWO_PREVIOUS))
+                }
+            }
+
+            if(page - 1 < 0) {
+                buttons.add(Button.of(ButtonStyle.SECONDARY, "prev", "Previous Pages", EmojiStore.PREVIOUS).asDisabled())
+            } else {
+                buttons.add(Button.of(ButtonStyle.SECONDARY, "prev", "Previous Pages", EmojiStore.PREVIOUS))
+            }
+
+            if(page + 1 >= totalPage) {
+                buttons.add(Button.of(ButtonStyle.SECONDARY, "next", "Next Page", EmojiStore.NEXT).asDisabled())
+            } else {
+                buttons.add(Button.of(ButtonStyle.SECONDARY, "next", "Next Page", EmojiStore.NEXT))
+            }
+
+            if(totalPage > 10) {
+                if(page + 10 >= totalPage) {
+                    buttons.add(Button.of(ButtonStyle.SECONDARY, "next10", "Next 10 Pages", EmojiStore.TWO_NEXT).asDisabled())
+                } else {
+                    buttons.add(Button.of(ButtonStyle.SECONDARY, "next10", "Next 10 Pages", EmojiStore.TWO_NEXT))
+                }
+            }
+
+            result.add(ActionRow.of(buttons))
         }
 
         result.add(ActionRow.of(Button.secondary("back", "Back").withEmoji(EmojiStore.BACK)))
