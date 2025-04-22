7563100326407d8efafb3aeb054ec37dac868990
MandarinSmell <geawonlee@naver.com>
2025-04-22 15:53:49 +0900

    remove warning

--- src/main/java/mandarin/packpack/commands/BoosterPin.java
+++ src/main/java/mandarin/packpack/commands/BoosterPin.java
@@ -9,7 +9,6 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
-import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.requests.RestAction;
 import org.jetbrains.annotations.Nullable;
