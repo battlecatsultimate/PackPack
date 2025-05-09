f4c5aae1483a432ed42c2f8e6c901111ba7ba9b7
MandarinSmell <geawonlee@naver.com>
2025-05-09 15:40:15 +0900

    remove unnecessary import

--- src/main/java/mandarin/packpack/supporter/server/holder/modal/ServerPrefixModalHolder.java
+++ src/main/java/mandarin/packpack/supporter/server/holder/modal/ServerPrefixModalHolder.java
@@ -2,7 +2,6 @@ package mandarin.packpack.supporter.server.holder.modal;
 
 import common.CommonStatic;
 import mandarin.packpack.supporter.lang.LangID;
-import mandarin.packpack.supporter.server.data.ConfigHolder;
 import mandarin.packpack.supporter.server.data.IDHolder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
