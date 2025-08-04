345669956964ca5e271f0aad4df87378f89ff88d
MandarinSmell <geawonlee@naver.com>
2025-08-04 18:29:11 +0900

    ignore warning

--- src/main/java/mandarin/packpack/supporter/StaticStore.java
+++ src/main/java/mandarin/packpack/supporter/StaticStore.java
@@ -178,10 +178,12 @@ public class StaticStore {
     };
 
     public static final int RED = 0;
+    @SuppressWarnings("unused")
     public static final int ORANGE = 1;
     public static final int YELLOW = 2;
     public static final int GREEN = 3;
     public static final int BLUE = 4;
+    @SuppressWarnings("unused")
     public static final int PURPLE = 5;
 
     public static final int[] grade = {
